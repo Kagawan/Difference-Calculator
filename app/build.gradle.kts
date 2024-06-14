5d022a6ac46eff6c314f4ec18b3247a1ea2d0365
rashid <rashid060803@mail.ru>
2024-06-14 11:37:47 +0300

    deleted spaces main.yml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,9 @@ repositories {
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("com.ginsberg:junit5-system-exit:1.1.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1") // последний раз добавил версию
+    //testImplementation("com.ginsberg:junit5-system-exit:1.1.2") // последний раз закоментировал
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher") // последний раз добавил
     implementation ("info.picocli:picocli:4.7.6")
     implementation("com.fasterxml.jackson.core:jackson-core:2.17.1")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.4")
@@ -46,3 +47,9 @@ tasks.jacocoTestReport { reports { xml.required.set(true) } }
 testlogger {
     showStandardStreams = true
 }
+
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
