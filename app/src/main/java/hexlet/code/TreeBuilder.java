862d39277535b60b7ba9962ddd43e7252b34bc5a
rashid <rashid060803@mail.ru>
2024-06-19 15:28:54 +0300

    changed main.yml

--- app/src/main/java/hexlet/code/TreeBuilder.java
+++ app/src/main/java/hexlet/code/TreeBuilder.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Objects;
 
-public class TreeBuilder {
+public class OutputStructure {
     public static List<Map<String, Object>> outputStructure(Map<String, Object> map1, Map<String, Object> map2) {
         List<Map<String, Object>> result = new ArrayList<>();
         Set<String> keys = new TreeSet<>(map1.keySet());
