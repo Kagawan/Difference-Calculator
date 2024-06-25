b13edaa4d014a65fa6b8a7e42eaf1228c0e58751
rashid <rashid060803@mail.ru>
2024-06-25 11:48:16 +0300

    corrected comments

--- app/src/test/java/hexlet/code/ApplicationTest.java
+++ app/src/test/java/hexlet/code/ApplicationTest.java
@@ -8,64 +8,64 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ApplicationTest {
     @Test
     public void testStylishFormatterJson() throws Exception {
-        String expected = readFromFile("src/test/resources/stylish.txt");
-        String actual = generate("src/test/resources/file1.json",
-                "src/test/resources/file2.json", "stylish");
+        String expected = readFromFile("src/test/resources/fixtures/stylish.txt");
+        String actual = generate("src/test/resources/fixtures/file1.json",
+                "src/test/resources/fixtures/file2.json", "stylish");
         assertEquals(expected, actual);
     }
     @Test
     public void testPlainFormatterJson() throws Exception {
-        String expected = readFromFile("src/test/resources/plain.txt");
-        String actual = generate("src/test/resources/file1.json",
-                "src/test/resources/file2.json", "plain");
+        String expected = readFromFile("src/test/resources/fixtures/plain.txt");
+        String actual = generate("src/test/resources/fixtures/file1.json",
+                "src/test/resources/fixtures/file2.json", "plain");
         assertEquals(expected, actual);
     }
 
     @Test
     public void testJsonFormatterJson() throws Exception {
-        String expected = readFromFile("src/test/resources/json.txt");
-        String actual = generate("src/test/resources/file1.json",
-                "src/test/resources/file2.json", "json");
+        String expected = readFromFile("src/test/resources/fixtures/json.txt");
+        String actual = generate("src/test/resources/fixtures/file1.json",
+                "src/test/resources/fixtures/file2.json", "json");
         assertEquals(expected, actual);
     }
 
     @Test
     public void testStylishFormatterYaml() throws Exception {
-        String expected = readFromFile("src/test/resources/stylish.txt");
-        String actual = generate("src/test/resources/file1.yaml",
-                "src/test/resources/file2.yaml", "stylish");
+        String expected = readFromFile("src/test/resources/fixtures/stylish.txt");
+        String actual = generate("src/test/resources/fixtures/file1.yaml",
+                "src/test/resources/fixtures/file2.yaml", "stylish");
         assertEquals(expected, actual);
     }
 
     @Test
     public void testPlainFormatterYaml() throws Exception {
-        String expected = readFromFile("src/test/resources/plain.txt");
-        String actual = generate("src/test/resources/file1.yaml",
-                "src/test/resources/file2.yaml", "plain");
+        String expected = readFromFile("src/test/resources/fixtures/plain.txt");
+        String actual = generate("src/test/resources/fixtures/file1.yaml",
+                "src/test/resources/fixtures/file2.yaml", "plain");
         assertEquals(expected, actual);
     }
 
     @Test
     public void testJsonFormatterYaml() throws Exception {
-        String expected = readFromFile("src/test/resources/json.txt");
-        String actual = generate("src/test/resources/file1.yaml",
-                "src/test/resources/file2.yaml", "json");
+        String expected = readFromFile("src/test/resources/fixtures/json.txt");
+        String actual = generate("src/test/resources/fixtures/file1.yaml",
+                "src/test/resources/fixtures/file2.yaml", "json");
         assertEquals(expected, actual);
     }
 
     @Test
     public void testGenerateJson() throws Exception {
-        String expected = readFromFile("src/test/resources/stylish.txt");
-        String actual = generate("src/test/resources/file1.json",
-                "src/test/resources/file2.json");
+        String expected = readFromFile("src/test/resources/fixtures/stylish.txt");
+        String actual = generate("src/test/resources/fixtures/file1.json",
+                "src/test/resources/fixtures/file2.json");
         assertEquals(expected, actual);
     }
 
     @Test
     public void testGenerateYaml() throws Exception {
-        String expected = readFromFile("src/test/resources/stylish.txt");
-        String actual = generate("src/test/resources/file1.yaml",
-                "src/test/resources/file2.yaml");
+        String expected = readFromFile("src/test/resources/fixtures/stylish.txt");
+        String actual = generate("src/test/resources/fixtures/file1.yaml",
+                "src/test/resources/fixtures/file2.yaml");
         assertEquals(expected, actual);
     }
 }
